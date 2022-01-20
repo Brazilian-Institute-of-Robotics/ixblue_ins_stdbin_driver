58ec4cb1b2cb7f81940af5eda6c7b67f1ffb2ff0
rafaelbarretorb <rafaelbarretorb@gmail.com>
2022-01-20 10:55:28 -0300

    add missing dvl msg advertise

--- ixblue_ins_driver/src/ros_publisher.cpp
+++ ixblue_ins_driver/src/ros_publisher.cpp
@@ -57,6 +57,7 @@ ROSPublisher::ROSPublisher() : nh("~"), diagPub(nh)
 
     // External Sensors
     stdSVSPublisher = nh.advertise<ixblue_ins_msgs::SVS>("ix/svs", 1);
+    stdDVLPublisher = nh.advertise<ixblue_ins_msgs::DVL>("ix/svl", 1);
 }
 
 void ROSPublisher::onNewStdBinData(
